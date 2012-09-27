113d79a883ba4a86346bbdd42639be1fc0874ff5
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-09-27 18:31:59 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manzier::Application.config.secret_token = '8c68b31f058e17d2eac4e08bbdf82538d77151278709153463dabc6ca480cb6518046a7e288b0b1268680c7b14a930eb2aae092c0969dec003b793f38254bf5f'
