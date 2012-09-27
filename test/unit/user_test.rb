ce8adbc4ecf4cf88217cd9abaf88a8b6996c5165
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-09-27 19:35:12 -0400

    Validated user has_many requests association.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  subject { users :valid }
+
+  should have_many :requests
 end
