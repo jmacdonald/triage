de9564341aa914d61c373f270f477005e250604c
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-09-28 01:40:12 -0400

    Added tests for default flag. Fixed other inconsistencies.

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -5,8 +5,8 @@ class StatusTest < ActiveSupport::TestCase
 
   should have_many :requests
   should validate_presence_of :title
-  should ensure_inclusion_of(:default).in_array ['true', 'false', 'asdf']
-  should ensure_inclusion_of(:closed).in_array ['true', 'false', 'asdf']
+  should ensure_inclusion_of(:default).in_array [true, false]
+  should ensure_inclusion_of(:closed).in_array [true, false]
 
   context 'a new default status' do
     setup do
@@ -17,11 +17,33 @@ class StatusTest < ActiveSupport::TestCase
       @status.save
     end
 
-    should 'should keep its default flag' do
+    should 'keep its default flag' do
       assert @status.default
     end
 
     should 'cause the previous default status to be unset' do
+      assert_false statuses(:valid).default?
+    end
+
+    should 'result in a single status being the default' do
+      assert_equal 1, Status.where(:default => true).count
+    end
+  end
+
+  context 'a new non-default status' do
+    setup do
+      # Create a new default status.
+      @status = Status.new
+      @status.title = 'Closed'
+      @status.default = false
+      @status.save
+    end
+
+    should 'not change the pre-existing default status' do
+      assert statuses(:valid).default?
+    end
+
+    should 'result in a single status being the default' do
       assert_equal 1, Status.where(:default => true).count
     end
   end
