b84eb5c219a51fe4445f64ba88b33ca6a9c2c373
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-09-29 02:28:27 -0400

    Added tests for provider role.

--- test/unit/ability_test.rb
+++ test/unit/ability_test.rb
@@ -26,4 +26,29 @@ class AbilityTest < ActiveSupport::TestCase
       assert @ability.can?(:manage, Status.new)
     end
   end
+
+  context 'a provider' do
+    setup do
+      @ability = Ability.new(users :provider)
+
+      @unassigned_request = Request.new
+
+      @assigned_request = Request.new
+      @assigned_request.assignee = users :provider
+    end
+
+    should 'be able to read any request' do
+      assert @ability.can?(:read, Request.new)
+    end
+
+    should 'only be able to manage requests assigned to it' do
+      assert @ability.can?(:manage, @assigned_request)
+      assert @ability.cannot?(:manage, @unassigned_request)
+    end
+
+    should 'not be able to destroy any requests' do
+      assert @ability.cannot?(:destroy, @unassigned_request)
+      assert @ability.cannot?(:destroy, @assigned_request)
+    end
+  end
 end
