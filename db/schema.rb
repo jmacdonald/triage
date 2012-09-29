e78ba8af4024948452272bcf873f46871c7b3719
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-09-29 00:33:31 -0400

    Added role column to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928131311) do
+ActiveRecord::Schema.define(:version => 20120928183306) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "request_id"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(:version => 20120928131311) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
