561ae383276f12dbb4c89a4f8be789794d0eb2da
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-09-28 21:06:52 -0400

    Moved tests into a context.

--- test/functional/requests_controller_test.rb
+++ test/functional/requests_controller_test.rb
@@ -5,18 +5,21 @@ class RequestsControllerTest < ActionController::TestCase
     sign_in users :valid
   end
 
-  test 'that the index action works' do
-    get :index
-    assert_response :success
-  end
+  context 'index action' do
+    setup do
+      get :index
+    end
 
-  test 'that users can only see their requests' do
-    get :index
-    assert_equal 1, assigns(:requests).count
-  end
+    should 'work' do
+      assert_response :success
+    end
 
-  test 'that assignments from other users are not shown as requests' do
-    get :index
-    assert_equal users(:valid), assigns(:requests).first.requester
+    should 'only show the current user\'s requests' do
+      assert_equal 1, assigns(:requests).count
+    end
+
+    should 'not show assignments' do
+      assert_equal users(:valid), assigns(:requests).first.requester
+    end
   end
 end
