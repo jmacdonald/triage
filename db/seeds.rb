7768942e2098d8e0a5e32d207eebcd724dd50192
Jordan MacDonald <jordan@wastedintelligence.com>
2012-10-04 02:40:41 -0400

    Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Status.create([{title: 'New', default: true}, {title: 'Assigned'}, {title: 'Closed'}])
