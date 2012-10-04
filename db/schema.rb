01e398d9bbb947c4eaad1b7bb832e3b8f1d8e745
Jordan MacDonald <jordan@wastedintelligence.com>
2012-10-04 03:10:11 -0400

    Added name column to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928183306) do
+ActiveRecord::Schema.define(:version => 20121004015926) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "request_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20120928183306) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "role"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
