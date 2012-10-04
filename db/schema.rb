888779a4d306634a69310ed90bd7543d25f85e75
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-10-04 18:13:39 -0400

    Ran rails_admin migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121004015926) do
+ActiveRecord::Schema.define(:version => 20121004191243) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "request_id"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(:version => 20121004015926) do
 
   add_index "comments", ["request_id"], :name => "index_comments_on_request_id"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "requests", :force => true do |t|
     t.string   "title"
