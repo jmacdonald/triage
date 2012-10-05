e68560b4457e3fcf0f86e29375d0a66beac74055
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-10-05 18:37:40 -0400

    Use cancan with rails_admin.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -20,6 +20,7 @@ RailsAdmin.config do |config|
   # or for a dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
+  config.authorize_with :cancan
 
   #  ==> Global show view settings
   # Display empty fields in show views
