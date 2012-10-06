b53cee36cd052b0145ce3ab9126bd80aef73c8a0
Jordan MacDonald <jordan@wastedintelligence.com>
2012-10-06 02:23:34 -0400

    Updated to turn status into a label.

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,10 @@
 module RequestsHelper
+  def label_class(status)
+    # Set the style based on the status type.
+    if status.default?
+      return 'label-success'
+    elsif status.closed?
+      return 'label-inverse'
+    end
+  end
 end
