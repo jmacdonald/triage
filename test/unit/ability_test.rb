3ab8085bb7063aee3bf3c44741a9af4154fcb627
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-10-09 18:22:38 -0400

    Updated ability test variables to better reflect their roles.

--- test/unit/ability_test.rb
+++ test/unit/ability_test.rb
@@ -3,33 +3,33 @@ require 'test_helper'
 class AbilityTest < ActiveSupport::TestCase
   context 'an administrator' do
     setup do
-      @ability = Ability.new(users :administrator)
+      @administrator = Ability.new(users :administrator)
     end
 
     should 'be able to manage any request' do
-      assert @ability.can?(:manage, Request.new)
+      assert @administrator.can?(:manage, Request.new)
     end
 
     should 'be able to manage any user' do
-      assert @ability.can?(:manage, User.new)
+      assert @administrator.can?(:manage, User.new)
     end
 
     should 'be able to manage any comment' do
-      assert @ability.can?(:manage, Comment.new)
+      assert @administrator.can?(:manage, Comment.new)
     end
 
     should 'be able to manage any attachment' do
-      assert @ability.can?(:manage, Attachment.new)
+      assert @administrator.can?(:manage, Attachment.new)
     end
 
     should 'be able to manage any status' do
-      assert @ability.can?(:manage, Status.new)
+      assert @administrator.can?(:manage, Status.new)
     end
   end
 
   context 'a provider' do
     setup do
-      @ability = Ability.new(users :provider)
+      @provider = Ability.new(users :provider)
 
       @unassigned_request = Request.new
 
@@ -41,34 +41,34 @@ class AbilityTest < ActiveSupport::TestCase
     end
 
     should 'be able to create new requests' do
-      assert @ability.can?(:create, @assigned_request)
+      assert @provider.can?(:create, @assigned_request)
     end
 
     should 'be able to read any request' do
-      assert @ability.can?(:read, @unassigned_request)
-      assert @ability.can?(:read, @assigned_request)
-      assert @ability.can?(:read, @admin_assigned_request)
+      assert @provider.can?(:read, @unassigned_request)
+      assert @provider.can?(:read, @assigned_request)
+      assert @provider.can?(:read, @admin_assigned_request)
     end
 
     should 'only be able to update requests assigned to it' do
-      assert @ability.can?(:update, @assigned_request)
-      assert @ability.cannot?(:update, @admin_assigned_request)
-      assert @ability.cannot?(:update, @unassigned_request)
+      assert @provider.can?(:update, @assigned_request)
+      assert @provider.cannot?(:update, @admin_assigned_request)
+      assert @provider.cannot?(:update, @unassigned_request)
     end
 
     should 'not be able to destroy any requests' do
-      assert @ability.cannot?(:destroy, @unassigned_request)
-      assert @ability.cannot?(:destroy, @assigned_request)
-      assert @ability.cannot?(:destroy, @admin_assigned_request)
+      assert @provider.cannot?(:destroy, @unassigned_request)
+      assert @provider.cannot?(:destroy, @assigned_request)
+      assert @provider.cannot?(:destroy, @admin_assigned_request)
     end
 
     should 'be able to comment on any request' do
-      assert @ability.can(:create, Comment.new)
+      assert @provider.can(:create, Comment.new)
     end
 
     should 'not be able to update or destroy comments' do
-      assert @ability.cannot?(:update, Comment.new)
-      assert @ability.cannot?(:destroy, Comment.new)
+      assert @provider.cannot?(:update, Comment.new)
+      assert @provider.cannot?(:destroy, Comment.new)
     end
   end
 
