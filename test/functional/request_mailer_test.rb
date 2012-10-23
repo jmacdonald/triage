33a09002af1731c1b54ed9c7b4d94c0ab35c32b5
Jordan MacDonald <jordan@wastedintelligence.com>
2012-10-22 23:46:09 -0400

    Added request mailer tests.

--- test/functional/request_mailer_test.rb
+++ test/functional/request_mailer_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class RequestMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'assignment email' do
+    setup do
+      @request = requests :valid
+      @email = RequestMailer.assignment_email(@request).deliver
+    end
+
+    should 'be queued for delivery' do
+      assert !ActionMailer::Base.deliveries.empty?
+    end
+
+    should 'be sent to the assignee' do
+      assert_equal [@request.assignee.email], @email.to
+    end
+
+    should 'have the right subject' do
+      assert_equal "You've been assigned a request", @email.subject
+    end
+  end
 end
