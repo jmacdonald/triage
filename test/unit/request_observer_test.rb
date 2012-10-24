d2e6b755a03c6c260db2a8f5f5273dcd7d72233f
Jordan MacDonald <jordan@wastedintelligence.com>
2012-10-23 23:10:21 -0400

    Added observer tests.

--- test/unit/request_observer_test.rb
+++ test/unit/request_observer_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class RequestObserverTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 're-assigning a request' do
+    setup do
+      request = requests :valid
+      new_assignee = users :provider
+
+      # Re-assign the request.
+      request.assignee = new_assignee
+      request.save
+    end
+
+    should 'send an email' do
+      # Ensure that an email is queued for delivery.
+      assert_equal 1, ActionMailer::Base.deliveries.size
+    end
+
+    should 'send the right email' do
+      # Ensure that the proper email is queued.
+      assert_equal "You've been assigned a request", ActionMailer::Base.deliveries[0].subject
+    end
+  end
 end
