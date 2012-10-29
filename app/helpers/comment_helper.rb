8fdf0bc1a5ac9aa0e0f068f464910844e2d05853
Jordan MacDonald <jordan@wastedintelligence.com>
2012-10-28 22:01:42 -0400

    Updated embolden_mentions to handle html escaping.

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -1,7 +1,14 @@
 module CommentHelper
   def embolden_mentions(content)
-    content.gsub /@\w+/ do |mention|
+    # Escape the html.
+    modified_content = ERB::Util.html_escape content
+
+    # Embolden the mentions.
+    modified_content.gsub! /@\w+/ do |mention|
       "<strong>#{mention}</strong>"
     end
+
+    # Return a version of the content that won't be escaped.
+    modified_content.html_safe
   end
 end
--- test/unit/helpers/comment_helper_test.rb
+++ test/unit/helpers/comment_helper_test.rb
@@ -6,12 +6,20 @@ class CommentHelperTest < ActionView::TestCase
       @user1 = users :administrator
       @user2 = users :provider
       @comment = comments :valid
+      @comment.content = "@#{@user1.username} can you talk to @#{@user2.username}?"
     end
 
     should 'embolden_mentions wraps mentions in strong tags' do
-      @comment.content = "@#{@user1.username} can you talk to @#{@user2.username}?"
-
       assert_equal "<strong>@#{@user1.username}</strong> can you talk to <strong>@#{@user2.username}</strong>?", embolden_mentions(@comment.content)
+    end
+
+    should 'return an html_safe string to prevent escaping' do
+      assert embolden_mentions(@comment.content).html_safe?
+    end
+
+    should 'escape html in the comment content' do
+      @comment.content = '<html>'
+      assert_equal '&lt;html&gt;', embolden_mentions(@comment.content)
     end
   end
 end
