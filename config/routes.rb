8836476aaf6cbb2379d81e829849d6d3c028c5f1
Curtis Schiewek <curtis.schiewek@gmail.com>
2012-11-04 12:58:50 -0500

    Refactored assignment routes into a scope, and removed superfluous routes.rb comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Triage::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   # Assignment routes. These are really just requests, but with some filters.
-  get 'assignments/open' => 'requests#open_assignments', :as => 'open_assignments'
-  get 'assignments/closed' => 'requests#closed_assignments', :as => 'closed_assignments'
+  scope 'assignments' do
+    get 'open' => 'requests#open_assignments', :as => 'open_assignments'
+    get 'closed' => 'requests#closed_assignments', :as => 'closed_assignments'
+  end
 
   resources :requests do
     get 'open', :on => :collection
@@ -18,60 +20,5 @@ Triage::Application.routes.draw do
     mount Notifier::Preview => 'mail_view'
   end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'requests#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
