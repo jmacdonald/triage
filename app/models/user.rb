4aaa1fdd88d90f23fed923f2abaadd1f1f683584
Jordan MacDonald <jordan@wastedintelligence.com>
2012-11-06 17:18:26 -0500

    Associated systems with users. First step towards resolving #12.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   has_many :assignments, :class_name => 'Request', :foreign_key => :assignee_id
   has_many :comments
   has_many :attachments
+  has_many :systems
 
   validates :username, :role, :name, :presence => true
   validates :username, :uniqueness => true, :format => /^\w+$/
