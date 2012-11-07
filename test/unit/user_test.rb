521971e1903f0cc0d3472defede66c4c4cb3a0d6
Jordan MacDonald <jordan@wastedintelligence.com>
2012-11-07 00:58:23 -0500

    Associated users and systems through responsibilities; working towards #12.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -7,6 +7,8 @@ class UserTest < ActiveSupport::TestCase
   should have_many :assignments
   should have_many :comments
   should have_many :attachments
+  should have_many :responsibilities
+  should have_many(:systems).through(:responsibilities)
   should validate_presence_of :email
   should validate_presence_of :username
   should validate_presence_of :role
