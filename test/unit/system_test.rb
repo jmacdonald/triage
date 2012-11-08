0dfc28ad899bffb5583ef75a5b0e6d9b959970c6
Jordan MacDonald <jordan@wastedintelligence.com>
2012-11-08 00:43:04 -0500

    Implemented algorithm to determine next assignee for a particular system. Working towards #12.

--- test/unit/system_test.rb
+++ test/unit/system_test.rb
@@ -12,4 +12,56 @@ class SystemTest < ActiveSupport::TestCase
   test 'that a system can display itself' do
     assert_equal 'Triage', systems(:valid).to_s
   end
+
+  context 'next_assignee method' do
+    setup do
+      # Remove any pre-existing requests; these can affect our tests.
+      Request.delete_all
+
+      # Define options to quickly generate requests.
+      @common_options = {
+        title: 'Test Request',
+        description: 'This is a test.',
+        requester_id: users(:requester).id,
+        system_id: systems(:valid).id,
+        severity: 'minor'
+      }
+
+      # Associate users with the system, clearing previous associations.
+      @system = systems :valid
+      @system.users = [users(:administrator), users(:provider)]
+      @system.save
+    end
+
+    should 'return nil if there are no users associated with this system' do
+      # Remove all users from the system.
+      @system.users = []
+      @system.save
+
+      assert_equal nil, @system.next_assignee
+    end
+
+    should 'return the first responsible user without any requests for this system' do
+      # Assign a request to the admin.
+      request = Request.new @common_options
+      request.assignee = users :administrator
+      request.save
+
+      assert_equal users(:provider), @system.next_assignee
+    end
+
+    should 'return the responsible user whose newest request for this system is the oldest' do
+      # Assign a request to the admin.
+      request = Request.new @common_options
+      request.assignee = users :administrator
+      request.save
+
+      # Assign a request to the provider.
+      request = Request.new @common_options
+      request.assignee = users :provider
+      request.save
+
+      assert_equal users(:administrator), @system.next_assignee
+    end
+  end
 end
