d73c9ba430b0489aef582c2959c9d812d3a3b307
Jordan MacDonald <jordan@wastedintelligence.com>
2012-11-07 21:14:01 -0500

    Removed user_id column, again. Last time, I swear.

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,6 @@ ActiveRecord::Schema.define(:version => 20121107051846) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
