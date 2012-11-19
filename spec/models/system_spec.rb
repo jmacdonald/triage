4860896f2b9a704ffddd438a4da920622b739790
Jordan MacDonald <jordan@wastedintelligence.com>
2012-11-18 23:39:19 -0500

    Updated auto assignment to be cognisant of users' availability. This (with the help of many commits before it) fixes #12.

--- spec/models/system_spec.rb
+++ spec/models/system_spec.rb
@@ -15,47 +15,95 @@ describe System do
     system.to_s.should eq(system.name)
   end
 
-  context 'next_assignee method' do
+  describe 'next_assignee method' do
     before(:each) do
       # Delete all prior requests.
       Request.delete_all
 
-      # Associate users with the system, clearing previous associations.
+      # Create a system.
       @system = FactoryGirl.create :system
-      @user1 = FactoryGirl.create :user
-      @user2 = FactoryGirl.create :user
-      @system.users = [@user1, @user2]
-      @system.save
     end
 
-    it 'should return nil if there are no users associated with this system' do
-      # Remove all users from the system.
-      @system.users = []
-      @system.save
-
-      @system.next_assignee.should be_nil
+    context 'when no users are responsible for this system' do
+      it 'should always return nil' do
+        @system.next_assignee.should be_nil
+      end
     end
 
-    it 'should return the first responsible user without any requests for this system' do
-      # Assign a request for this system to the first user.
-      request = FactoryGirl.build :request
-      request.system = @system
-      request.assignee = @user1
-      request.save
-
-      @system.next_assignee.should eq(@user2)
-    end
-
-    it 'should return the responsible user whose newest request for this system is the oldest' do
-      # Assign requests to all users.
-      [@user1, @user2].each do |user|
-        request = FactoryGirl.build :request
-        request.system = @system
-        request.assignee = user
-        request.save
+    context 'when there are multiple users responsible for this system' do
+      before(:each) do
+        # Associate users with the system.
+        @user1 = FactoryGirl.create :user
+        @user2 = FactoryGirl.create :user
+        @system.users = [@user1, @user2]
+        @system.save
       end
 
-      @system.next_assignee.should eq(@user1)
+      context 'when only one of the users has had a request assigned to it' do
+        before :each do
+          # Assign a request for this system to the first user.
+          request = FactoryGirl.build :request
+          request.system = @system
+          request.assignee = @user1
+          request.save
+        end
+
+        it 'should return the first responsible user without any requests for this system' do
+          @system.next_assignee.should eq(@user2)
+        end
+
+        it 'should return the first user if the second is unavailable' do
+          # Set the second user as unavailable.
+          @user2.available = false
+          @user2.save
+
+          @system.next_assignee.should eq(@user1)
+        end
+
+        it 'should not return anything when all users are unavailable' do
+          # Set all of the users to unavailable.
+          @user1.available = false
+          @user2.available = false
+          @user1.save
+          @user2.save
+
+          @system.next_assignee.should be_nil
+        end
+      end
+
+      context 'when assigned users have all been assigned a request for it at some point' do
+        before(:each) do
+          # Assign requests to all users.
+          [@user1, @user2].each do |user|
+            request = FactoryGirl.build :request
+            request.system = @system
+            request.assignee = user
+            request.save
+          end
+        end
+
+        it 'should return the responsible user whose newest request for this system is the oldest' do
+          @system.next_assignee.should eq(@user1)
+        end
+
+        it 'should return the second user if the first is unavailable' do
+          # Set the first user as unavailable.
+          @user1.available = false
+          @user1.save
+
+          @system.next_assignee.should eq(@user2)
+        end
+
+        it 'should not return anything when all users are unavailable' do
+          # Set all of the users to unavailable.
+          @user1.available = false
+          @user2.available = false
+          @user1.save
+          @user2.save
+
+          @system.next_assignee.should be_nil
+        end
+      end
     end
   end
 end
