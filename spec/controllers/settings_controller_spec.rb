f394d65f6a001a6feadbae7676460d18fa9d4af9
Jordan MacDonald <jordan@wastedintelligence.com>
2012-12-04 00:39:01 -0500

    Updated to authenticate and authorize edit_profile action.

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe SettingsController do
+  before(:each) do
+    @current_user = FactoryGirl.create :user, role: 'administrator'
+    sign_in @current_user
+  end
 
   describe "GET 'edit_profile'" do
     it "returns http success" do
@@ -8,5 +12,4 @@ describe SettingsController do
       response.should be_success
     end
   end
-
 end
