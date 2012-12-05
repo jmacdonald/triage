6c3fc211cda5eb935933e0c04a365de59c03e631
Jordan MacDonald <jordan@wastedintelligence.com>
2012-12-04 22:45:40 -0500

    Removed mass assignment tests and accessible attributes from user model. Removed whitelisting of attributes throughout app. Work towards #27.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ActiveRecord::Base
 
   devise :ldap_authenticatable, :database_authenticatable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :name, :available, :role
-
   has_many :requests, :foreign_key => :requester_id
   has_many :assignments, :class_name => 'Request', :foreign_key => :assignee_id
   has_many :comments
