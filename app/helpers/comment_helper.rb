e444c325822442cb4ebcaaa2c3721b30f59e2ae0
Jordan MacDonald <jordan@wastedintelligence.com>
2012-12-21 22:02:20 -0500

    Moved html escaping into a separate helper.

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -1,14 +1,13 @@
 module CommentHelper
-  def embolden_mentions(content)
-    # Escape the html.
-    modified_content = ERB::Util.html_escape content
+  def escape(content)
+    # Escape the html and flag it as safe.
+    ERB::Util.html_escape(content).html_safe
+  end
 
+  def embolden_mentions(content)
     # Embolden the mentions.
-    modified_content.gsub! /@\w+/ do |mention|
+    content.gsub! /@\w+/ do |mention|
       "<strong>#{mention}</strong>"
     end
-
-    # Return a version of the content that won't be escaped.
-    modified_content.html_safe
   end
 end
--- spec/helpers/comment_helper_spec.rb
+++ spec/helpers/comment_helper_spec.rb
@@ -8,20 +8,23 @@ describe CommentHelper do
     @comment.content = content
   end
 
+  describe 'escape' do
+    let (:content) { '<html>' }
+
+    it 'should escape html in the comment content' do
+      assert_equal '&lt;html&gt;', escape(@comment.content)
+    end
+
+    it 'should return an html_safe string to prevent escaping' do
+      assert escape(@comment.content).html_safe?
+    end
+  end
+
   describe 'embolden_mentions' do
     let(:content) { "@#{@user1.username} can you talk to @#{@user2.username}?" }
 
     it 'should wrap mentions in strong tags' do
       assert_equal "<strong>@#{@user1.username}</strong> can you talk to <strong>@#{@user2.username}</strong>?", embolden_mentions(@comment.content)
-    end
-
-    it 'should return an html_safe string to prevent escaping' do
-      assert embolden_mentions(@comment.content).html_safe?
-    end
-
-    it 'should escape html in the comment content' do
-      @comment.content = '<html>'
-      assert_equal '&lt;html&gt;', embolden_mentions(@comment.content)
     end
   end
 end
