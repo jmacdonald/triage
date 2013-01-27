b945b7f0c8e1bf26492ae1b9ead89924b71401f4
Jordan MacDonald <jordan@wastedintelligence.com>
2013-01-27 17:33:26 -0500

    Dropped rails admin histories table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121118164749) do
+ActiveRecord::Schema.define(:version => 20130127223109) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "request_id"
@@ -37,19 +37,6 @@ ActiveRecord::Schema.define(:version => 20121118164749) do
 
   add_index "comments", ["request_id"], :name => "index_comments_on_request_id"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
-
-  create_table "rails_admin_histories", :force => true do |t|
-    t.text     "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
-  end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "requests", :force => true do |t|
     t.string   "title"
@@ -87,22 +74,25 @@ ActiveRecord::Schema.define(:version => 20121118164749) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",               :default => "",   :null => false
-    t.string   "encrypted_password",  :default => "",   :null => false
+    t.string   "email",                  :default => "",   :null => false
+    t.string   "encrypted_password",     :default => "",   :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
     t.string   "role"
     t.string   "name"
     t.string   "username"
-    t.boolean  "available",           :default => true
+    t.boolean  "available",              :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
