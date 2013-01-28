a55a9569e9794863a3a6ae644b2108a7021f2b68
Jordan MacDonald <jordan@wastedintelligence.com>
2013-01-28 00:49:52 -0500

    Added responsibility editing to system edit screen. Fixes #35.

--- app/admin/systems.rb
+++ app/admin/systems.rb
@@ -5,4 +5,15 @@ ActiveAdmin.register System do
     column :name
     default_actions
   end
+
+  form do |f|
+    f.inputs "System" do
+      f.input :name
+    end
+
+    f.inputs "Responsible Users" do
+      f.input :users
+    end
+    f.buttons
+  end
 end
