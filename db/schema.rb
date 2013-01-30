574b38ed700112404baa734ba6ff78c027cb5556
Jordan MacDonald <jordan@wastedintelligence.com>
2013-01-30 16:17:14 -0500

    Added type column to user table for STI.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130127223109) do
+ActiveRecord::Schema.define(:version => 20130130211639) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "request_id"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(:version => 20130127223109) do
     t.string   "name"
     t.string   "username"
     t.boolean  "available",              :default => true
+    t.string   "type"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
