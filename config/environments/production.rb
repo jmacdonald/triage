768259f6b11bbaf0c528d38530897949ab186911
Jordan MacDonald <jordan@wastedintelligence.com>
2013-02-13 18:32:03 -0500

    Added default SMTP settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,15 @@ Triage::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'localhost',
+    :port                 => 587,
+    :user_name            => 'triage',
+    :password             => 'password',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 end
