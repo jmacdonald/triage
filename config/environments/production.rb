30610f26fb746a4467f214b8ac2b41b42a5e3637
Jordan MacDonald <jordan@wastedintelligence.com>
2013-05-25 15:52:17 -0400

    Added default mailer url for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Triage::Application.configure do
     :authentication       => 'plain',
     :enable_starttls_auto => true
   }
+
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 end
