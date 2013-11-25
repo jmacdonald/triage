5ea67fe138f359f98838b394adadf9cf703c6cec
Jordan MacDonald <jordan@wastedintelligence.com>
2013-11-24 20:50:59 -0500

    Rails 4 schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -9,14 +9,17 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130130211639) do
+ActiveRecord::Schema.define(version: 20130130211639) do
 
-  create_table "attachments", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attachments", force: true do |t|
     t.integer  "request_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "file_file_name"
     t.string   "file_content_type"
     t.integer  "file_file_size"
@@ -25,75 +28,75 @@ ActiveRecord::Schema.define(:version => 20130130211639) do
     t.string   "title"
   end
 
-  add_index "attachments", ["request_id"], :name => "index_attachments_on_request_id"
+  add_index "attachments", ["request_id"], name: "index_attachments_on_request_id", using: :btree
 
-  create_table "comments", :force => true do |t|
+  create_table "comments", force: true do |t|
     t.text     "content"
     t.integer  "user_id"
     t.integer  "request_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["request_id"], :name => "index_comments_on_request_id"
-  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+  add_index "comments", ["request_id"], name: "index_comments_on_request_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
-  create_table "requests", :force => true do |t|
+  create_table "requests", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "requester_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "assignee_id"
     t.integer  "status_id"
     t.integer  "system_id"
     t.string   "severity"
   end
 
-  add_index "requests", ["requester_id"], :name => "index_requests_on_user_id"
+  add_index "requests", ["requester_id"], name: "index_requests_on_user_id", using: :btree
 
-  create_table "responsibilities", :force => true do |t|
+  create_table "responsibilities", force: true do |t|
     t.integer  "user_id"
     t.integer  "system_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "statuses", :force => true do |t|
+  create_table "statuses", force: true do |t|
     t.string   "title"
-    t.boolean  "default",    :default => false
-    t.boolean  "closed",     :default => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.boolean  "default",    default: false
+    t.boolean  "closed",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  create_table "systems", :force => true do |t|
+  create_table "systems", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",   :null => false
-    t.string   "encrypted_password",     :default => "",   :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.string   "role"
     t.string   "name"
     t.string   "username"
-    t.boolean  "available",              :default => true
+    t.boolean  "available",              default: true
     t.string   "type"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
