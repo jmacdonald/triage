80a6b5509518f5a4e01cc715c61371bf747b8705
Jordan MacDonald <jordan@wastedintelligence.com>
2013-11-25 21:11:09 -0500

    Set a default secret base.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Triage::Application.config.secret_token = '8c68b31f058e17d2eac4e08bbdf82538d77151278709153463dabc6ca480cb6518046a7e288b0b1268680c7b14a930eb2aae092c0969dec003b793f38254bf5f'
+Triage::Application.config.secret_key_base = '23501d22906695a6f6ab02400139422edd0d084fb91a28acd36be0be9483db3a22b028e0d4b60494ebebba223694a5410fcec743fb8280417a1309142a3e1f26'
